aa4ecf7c05f58bf3364492f22946395a6fdec37f
turingcompl33t <kdotterrer@gmail.com>
2021-01-02 18:11:02 -0500

    rendezvous channel

--- projects/channel/src/rendezvous.rs
+++ projects/channel/src/rendezvous.rs
@@ -1,20 +1,20 @@
 // rendezvous.rs
 // Rendezvous channel, like a bounded mpsc channel with 0 internal capacity.
 
-use std::sync::{Arc, Mutex, Condvar};
+use std::sync::{Arc, Condvar, Mutex};
 
 struct Inner<T> {
     data: Option<T>,
     n_txs: usize,
-    n_rxs: usize
+    n_rxs: usize,
 }
 
 impl<T> Inner<T> {
-    fn new() -> Self {
+    fn with_refcounts(n_txs: usize, n_rxs: usize) -> Self {
         Self {
             data: None,
-            n_txs: 1,
-            n_rxs: 1
+            n_txs,
+            n_rxs,
         }
     }
 }
@@ -22,45 +22,46 @@ impl<T> Inner<T> {
 struct Shared<T> {
     inner: Mutex<Inner<T>>,
     tx_ok: Condvar,
-    rx_ok: Condvar
+    rx_ok: Condvar,
 }
 
 impl<T> Shared<T> {
     fn new() -> Self {
         Self {
-            inner: Mutex::new(Inner::<T>::new()),
+            inner: Mutex::new(Inner::<T>::with_refcounts(1, 1)),
             tx_ok: Condvar::new(),
-            rx_ok: Condvar::new()
+            rx_ok: Condvar::new(),
         }
     }
 }
 
 pub struct Sender<T> {
-    shared: Arc<Shared<T>>
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Sender<T> {
     pub fn send(&mut self, data: T) -> Result<(), T> {
         let mut inner = self.shared.inner.lock().unwrap();
         if 0 == inner.n_rxs {
-            return Result::Err(data);
+            return Err(data);
         }
 
         loop {
             match inner.data {
-                None => break,
+                None if 0 == inner.n_rxs => {
+                    return Err(data);
+                }
+                None => {
+                    inner.data = Some(data);
+                    drop(inner);
+                    self.shared.rx_ok.notify_one();
+                    return Ok(());
+                }
                 Some(_) => {
                     inner = self.shared.tx_ok.wait(inner).unwrap();
                 }
             }
         }
-
-        inner.data = Some(data);
-
-        drop(inner);
-        self.shared.rx_ok.notify_one();
-
-        Ok(())
     }
 }
 
@@ -71,19 +72,74 @@ impl<T> Clone for Sender<T> {
         drop(inner);
 
         Self {
-            shared: Arc::clone(&self.shared)
+            shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Sender<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.n_txs -= 1;
+        let last_out = 0 == inner.n_rxs;
+        drop(inner);
+        if last_out {
+            self.shared.rx_ok.notify_all();
         }
     }
 }
 
 pub struct Receiver<T> {
-    shared: Arc<Shared<T>>
+    shared: Arc<Shared<T>>,
 }
 
 impl<T> Receiver<T> {
     pub fn recv(&mut self) -> Result<T, ()> {
-        let mut _inner = self.shared.inner.lock().unwrap();
-        Err(())
+        let mut inner = self.shared.inner.lock().unwrap();
+        if 0 == inner.n_txs {
+            return Err(());
+        }
+
+        loop {
+            match inner.data {
+                Some(_) => {
+                    let t = inner.data.take().unwrap();
+                    drop(inner);
+                    self.shared.tx_ok.notify_one();
+                    return Ok(t);
+                }
+                None if 0 == inner.n_txs => {
+                    return Err(());
+                }
+                None => {
+                    inner = self.shared.rx_ok.wait(inner).unwrap();
+                }
+            }
+        }
+    }
+}
+
+impl<T> Clone for Receiver<T> {
+    fn clone(&self) -> Self {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.n_rxs += 1;
+        drop(inner);
+
+        Self {
+            shared: Arc::clone(&self.shared),
+        }
+    }
+}
+
+impl<T> Drop for Receiver<T> {
+    fn drop(&mut self) {
+        let mut inner = self.shared.inner.lock().unwrap();
+        inner.n_rxs -= 1;
+        let last_out = 0 == inner.n_rxs;
+        drop(inner);
+        if last_out {
+            self.shared.tx_ok.notify_all();
+        }
     }
 }
 
@@ -92,11 +148,11 @@ pub fn rendezvous<T>() -> (Sender<T>, Receiver<T>) {
 
     (
         Sender {
-            shared: Arc::clone(&shared)
+            shared: Arc::clone(&shared),
         },
         Receiver {
-            shared: Arc::clone(&shared)
-        }
+            shared: Arc::clone(&shared),
+        },
     )
 }
 
@@ -107,5 +163,26 @@ mod tests {
     #[test]
     fn it_works() {
         let (_, _) = rendezvous::<()>();
+    }
+
+    #[test]
+    fn basic() {
+        let (mut tx, mut rx) = rendezvous::<()>();
+        assert_eq!(tx.send(()), Ok(()));
+        assert_eq!(rx.recv(), Ok(()));
+    }
+
+    #[test]
+    fn tx_drops() {
+        let (tx, mut rx) = rendezvous::<()>();
+        drop(tx);
+        assert_eq!(rx.recv(), Err(()));
+    }
+
+    #[test]
+    fn rx_drops() {
+        let (mut tx, rx) = rendezvous::<()>();
+        drop(rx);
+        assert_eq!(tx.send(()), Err(()));
     }
 }
